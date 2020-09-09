2046a093491cb74704b9d1e05cc57596c44bce3c
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-09-08 23:36:11 -0300

    Update PalavraAleatoria.java

--- games_simples/PalavraAleatoria.java
+++ games_simples/PalavraAleatoria.java
@@ -3,17 +3,22 @@ package games_simples;
 import java.util.Random;
 import java.util.Scanner;
 
+package games_simples;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class PalavraAleatoria {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
-		String[] palavras = { "computador"};//, "livro", //"microfone", "machado", ""};
+		String[] palavras = { "computador",  "livro"};//"microfone", "machado", ""};
 		Random random = new Random();
 		
 		short chances = 3;
 		String answer;
-		System.out.print("Digite s para começar a jogar: ");
+		System.out.print("Digite s para comeÃ§ar a jogar: ");
 		in.next().charAt(0);
 		String palavrasAleatorias = palavras[random.nextInt(palavras.length)];
 		
@@ -24,6 +29,24 @@ public class PalavraAleatoria {
 				answer = in.next();
 				if (answer.equals("computador") || answer.equals("Computador")) {
 					System.out.println("Voce acertou!!! A palavra eh: computador");
+				} else {
+					System.out.print("Voce errou, digite outra palavra: ");
+					chances -= 1;
+					answer = in.nextLine();
+				}
+				if (chances == 0) {
+				System.out.println("Suas tentativas acabaram");
+				break;
+				}
+			}
+		}
+		if (palavrasAleatorias.equals("livro")) {
+			System.out.print("Dicas: voce pode usar para se entreter, pesquisar etc. \nPode durar muito tempo \nTodo mundo tem pelo menos um em casa. "
+			+ "\nInsira a resposta: ");
+			while (chances <= 3) {
+				answer = in.next();
+				if (answer.equals("livro") || answer.equals("Livro")) {
+					System.out.println("Voce acertou!!! A palavra eh: livro");
 				} else {
 					System.out.print("Voce errou, digite outra palavra: ");
 					chances -= 1;
