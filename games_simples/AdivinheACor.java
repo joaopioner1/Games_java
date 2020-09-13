5a06d087ebe6691ea90b47d9a65b2d8f1c9193c4
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-09-13 16:52:02 -0300

    Versao final do game adivinhe a cor.

--- games_simples/AdivinheACor.java
+++ games_simples/AdivinheACor.java
@@ -8,19 +8,25 @@ public class AdivinheACor {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Random random = new Random();
-		String[] colors = {"Vermelho", "Amarelo", "Verde", "Laranja", "Roxo", "Rosa", "Vinho"
+		String[] colors = {"Vermelho", "Amarelo", "Verde", "Laranja", "Roxo", "Rosa", "Vinho",
 		"Azul", "Branco", "Preto", "Marrom"}; 
+		String randomColor = colors[random.nextInt(colors.length)];
 		
 		System.out.print("Insira sua cor favorita e eu tentarei adivinhar(apenas cores com uma palavra): ");
-		String input = in.next();
-		char answer;
+		in.next();
 		
-		while (answer != 's') {
-			String randomColor = colors[random.nextInt(colors.length)];
-			System.out.print("Sua cor eh " + randomColor + " (Digite s ou n)? ");
-			answer = in.next().charAt(0);
+		System.out.print("Sua cor eh " + randomColor + " (s/n)? ");
+		char answer = in.next().charAt(0);
+		if (answer == 's') {
+			System.out.println("Acertei sua cor. Haha");
+		} else {
+			while (answer != 's') {
+				randomColor = colors[random.nextInt(colors.length)];
+				System.out.print("Sua cor eh " + randomColor + " (Digite s ou n)? ");
+				answer = in.next().charAt(0);
+			}
 		}
-		System.out.println("Acertei sua cor. haha");
+		System.out.println("Acertei sua cor. Haha");
 		
 		in.close();
 	}
