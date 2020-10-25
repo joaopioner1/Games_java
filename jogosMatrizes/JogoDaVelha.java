25746227c736b1275515d537c966968fc229b341
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-24 22:02:04 -0300

    Atualizei o jogo da velha

--- jogosMatrizes/JogoDaVelha.java
+++ jogosMatrizes/JogoDaVelha.java
@@ -6,12 +6,14 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class JogoDaVelha extends JPanel implements MouseListener{ //Ler as acoes do mouse
 
 		//Autor: Joao Vitor Souza Pioner Data: 22/10/2020 7:36 h
 		Font minhaFont = new Font("Consolas", Font.BOLD, 80);
+		Font smallFont = new Font("Consolas", Font.BOLD, 40);
 	
 		int matriz[][];
 		int jogador, ganhador = 0;
@@ -41,7 +43,7 @@ public class JogoDaVelha extends JPanel implements MouseListener{ //Ler as acoes
 	
 			g.setFont(minhaFont);
 			
-			g.setColor(Color.red);
+			g.setColor(Color.LIGHT_GRAY);
 			g.fillRect(0, 0, 600, 600);
 			
 			g.setColor(Color.black); //linhas
@@ -59,8 +61,15 @@ public class JogoDaVelha extends JPanel implements MouseListener{ //Ler as acoes
 					} else if (matriz[lin][col] == 2) {
 					g.drawString("x", 75 + col * 200, 110 + lin * 200); // "" + transformou em String
 					}
+				}
 			}
-		}
+			
+			if (ganhador != 0) {
+				g.setColor(Color.blue);
+				g.setFont(smallFont);
+				g.drawString("O jogador " + ganhador + " venceu", 100, 170);
+				
+			}
 	}
 
 		@Override
@@ -86,14 +95,33 @@ public class JogoDaVelha extends JPanel implements MouseListener{ //Ler as acoes
 		private void verificaGanhador() {
 			
 			for (int lin = 0; lin < matriz.length; lin++) {
-					if (matriz[lin][0] == matriz[lin][1] && matriz[lin][0] == matriz[0][2] && matriz[lin][0] != 0) {
+					if (matriz[lin][0] == matriz[lin][1] && matriz[lin][0] == matriz[lin][2] && matriz[lin][0] != 0) {
 						System.out.println("Houve ganhador");
 						ganhador = matriz[lin][0];
 						break;
 					}
-				
+				}
+			
+			for (int col = 0; col < matriz.length; col++) {
+				if (matriz[0][1] == matriz[1][col] && matriz[0][col] == matriz[2][col] && matriz[0][col] != 0) {
+					System.out.println("Houve ganhador");
+					ganhador = matriz[col][0];
+					break;
+				}
+			}
+			
+			if (matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2] && matriz[0][0] != 0) {
+				System.out.println("Houve ganhador");
+				ganhador = matriz[0][0];
+
+			}
+			
+			if (matriz[0][2] == matriz[1][1] && matriz[0][2] == matriz[2][0] && matriz[0][2] != 0) {
+				System.out.println("Houve ganhador");
+				ganhador = matriz[0][0];
+
+			}
 		}
-	}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
